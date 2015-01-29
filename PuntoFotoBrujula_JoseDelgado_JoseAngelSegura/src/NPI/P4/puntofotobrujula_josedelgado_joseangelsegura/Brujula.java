eb20e71c8a68409d9100a728f90515fb65edd2cf
PPeMg <jdeldo@gmail.com>
2015-01-29 01:12:45 +0100

    Añadida la licencia
    Añadida la licencia a la brújula

--- PuntoFotoBrujula_JoseDelgado_JoseAngelSegura/src/NPI/P4/puntofotobrujula_josedelgado_joseangelsegura/Brujula.java
+++ PuntoFotoBrujula_JoseDelgado_JoseAngelSegura/src/NPI/P4/puntofotobrujula_josedelgado_joseangelsegura/Brujula.java
@@ -1,23 +1,39 @@
+/*
+ *	Copyright 2014 José Delgado Dolset and Jose Angel Segura Muros.
+ *
+ *	This file is part of the fourth practise in the subject of
+ *	"Nuevos Paradigmas de la Interacción" in the Computer Engineering's
+ *	Degree from University of Granada.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package NPI.P4.puntofotobrujula_josedelgado_joseangelsegura;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import android.support.v7.app.ActionBarActivity;
 import android.annotation.TargetApi;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageButton;
@@ -29,7 +45,7 @@ import android.widget.ImageView;
  * funcionamiento:
  * 
  * @author José Delgado Dolset
- * @author José Ángel Segura Moya
+ * @author José Ángel Segura Muros
  * @see https://github.com/DavidGasquez/windrose
  * 
  */
--- PuntoFotoBrujula_JoseDelgado_JoseAngelSegura/src/NPI/P4/puntofotobrujula_josedelgado_joseangelsegura/ComprobadorDireccion.java
+++ PuntoFotoBrujula_JoseDelgado_JoseAngelSegura/src/NPI/P4/puntofotobrujula_josedelgado_joseangelsegura/ComprobadorDireccion.java
@@ -1,3 +1,25 @@
+/*
+ *	Copyright 2014 José Delgado Dolset and Jose Angel Segura Muros.
+ *
+ *	This file is part of the fourth practise in the subject of
+ *	"Nuevos Paradigmas de la Interacción" in the Computer Engineering's
+ *	Degree from University of Granada.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package NPI.P4.puntofotobrujula_josedelgado_joseangelsegura;
 
 public class ComprobadorDireccion {
